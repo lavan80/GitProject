bc8163060bfe66301c0896d5e2ca115449119ae2
lavan80 <andrei.vas1080@gmail.com>
2022-03-13 10:06:07 +0200

    drawable animation

---
+++ app/src/main/java/com/test/gitproject/ViewBackgroundAnimationActivity.kt
@@ -0,0 +1,19 @@
+package com.test.gitproject
+
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.Button
+
+class ViewBackgroundAnimationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_background_animation)
+        val button = findViewById<Button>(R.id.bt_start)
+        val animationDrawable = button.background as AnimationDrawable
+            button.setOnClickListener {
+                animationDrawable.start()
+        }
+    }
+}
